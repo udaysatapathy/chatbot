b07bf9492950279c6c143db6465b98efd8bd1c80
Uday Satapathy <60004055+udaysatapathy@users.noreply.github.com>
2020-05-17 15:57:24 -0400

    Update and rename main_bot.py to simple_test_bot.py

--- simple_test_bot.py
+++ simple_test_bot.py
@@ -76,18 +76,10 @@ def main():
             return
         token = os.environ["TELEGRAM_TOKEN"]
 
-    #################################################################
-    
-    # Your task is to complete dialogue_manager.py and use your 
-    # advanced DialogueManager instead of SimpleDialogueManager. 
-    
-    # This is the point where you plug it into the Telegram bot. 
-    # Do not forget to import all needed dependencies when you do so.
+    # initializing the simple dialog manager just to test Telegram connections
     
     simple_manager = SimpleDialogueManager()
     bot = BotHandler(token, simple_manager)
-    
-    ###############################################################
 
     print("Ready to talk!")
     offset = 0
